f3dc1e639924a475f2e57a3d961ee3a34f7f6121
andres <anacleto115@hotmail.com>
2025-03-10 18:42:32 -0500

    Base 10-03-2025

---
+++ main.py
@@ -0,0 +1,20 @@
+import datetime;
+import decimal;
+
+class Estados:
+  id: int = 0;
+
+  def GetId(self) -> int:
+    return self.id;
+  def SetId(self, value: int) -> None:
+    self.id = value;
+
+  nombre: str = None;
+
+  def GetNombre(self) -> str:
+    return self.nombre;
+  def SetNombre(self, value: str) -> None:
+    self.nombre = value;
+
+estado = Estados();
+print(estado.GetNombre());
