337b5fb275eb9dd4ad0d0fcfcce15e0bb46ff10e
andres <Anacleto115@hotmail.com>
2025-03-10 21:28:08 -0500

    Clase 10-03-2025 9:28

--- main.py
+++ main.py
@@ -28,6 +28,56 @@ class Estados:
 	def SetNombre(self, value: str) -> None:
 		self.nombre = value;
 
+class Personas:
+	id: int = 0;
+
+	def GetId(self) -> int:
+		return self.id;
+	def SetId(self, value: int) -> None:
+		self.id = value;
+
+	cedula: str = None;
+
+	def GetCedula(self) -> str:
+		return self.cedula;
+	def SetCedula(self, value: str) -> None:
+		self.cedula = value;
+
+	nombre: str = None;
+
+	def GetNombre(self) -> str:
+		return self.nombre;
+	def SetNombre(self, value: str) -> None:
+		self.nombre = value;
+
+	estado: int = None;
+
+	def GetEstado(self) -> int:
+		return self.estado;
+	def SetEstado(self, value: int) -> None:
+		self.estado = value;
+
+	fecha: datetime = None;
+
+	def GetFecha(self) -> datetime:
+		return self.fecha;
+	def SetFecha(self, value: datetime) -> None:
+		self.fecha = value;
+
+	activo: bool = None;
+
+	def GetActivo(self) -> bool:
+		return self.activo;
+	def SetActivo(self, value: bool) -> None:
+		self.activo = value;
+
+	_estado: Estados = None;
+
+	def Get_Estado(self) -> Estados:
+		return self._estado;
+	def Set_Estado(self, value: Estados) -> None:
+		self._estado = value;
+
 # REPOSITORIO
 class Repositorio:
 	strConnection: str = """
@@ -71,13 +121,46 @@ class Repositorio:
 		for estado in lista:
 			print(str(estado.GetId()) + ", " + estado.GetNombre());
 
+	def ConexionBasica3(self) -> None:
+		conexion = pyodbc.connect(self.strConnection);
+
+		consulta: str = """SELECT p.id,
+				p.cedula,
+				p.nombre,
+				p.estado,
+				p.fecha,
+				p.activo,
+				e.id,
+				e.nombre
+			FROM `personas` p INNER JOIN `estados` e ON p.estado = e.id;""";
+		cursor = conexion.cursor();
+		cursor.execute(consulta);
+
+		for elemento in cursor:
+			print(elemento);
+
+		cursor.close();
+		conexion.close();
+
+	def ConexionBasica4(self) -> None:
+		conexion = pyodbc.connect(self.strConnection);
+
+		consulta: str = """INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Test');""";
+		cursor = conexion.cursor();
+		cursor.execute(consulta);
+		cursor.commit();
+
+		cursor.close();
+		conexion.close();
 
 estado = Estados();
 print(estado.GetNombre());
 
 repositorio = Repositorio();
 # repositorio.ConexionBasica();
-repositorio.ConexionBasica2();
+# repositorio.ConexionBasica2();
+# repositorio.ConexionBasica3();
+repositorio.ConexionBasica4();
 
 """
 VERSION DE PYTHON
