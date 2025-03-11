a71df05c39bb4469ec2c9e3deceb4153e369c04f
andres <Anacleto115@hotmail.com>
2025-03-10 21:17:12 -0500

    Clase 10-03-2025 9:17

--- script.sql
+++ script.sql
@@ -9,8 +9,24 @@ CREATE TABLE `db_personas`.`estados` (
     PRIMARY KEY(`id`)
 );
 
+CREATE TABLE `db_personas`.`personas` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `cedula` VARCHAR(20) NOT NULL UNIQUE,
+    `nombre` VARCHAR(250) NOT NULL,
+    `estado` INT NOT NULL,
+    `fecha` DATETIME NOT NULL,
+    `activo` BIT NOT NULL,
+    PRIMARY KEY(`id`),
+    CONSTRAINT `fk_personas__estados` FOREIGN KEY (`estado`) REFERENCES `estados`(`id`)
+);
+
 INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Casad@s');
 INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Solter@s');
 INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Vioud@s');
 INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Separados@s');
 INSERT INTO `db_personas`.`estados` (`nombre`) VALUES ('Lo que importa es que Diosito me quiere');
+
+INSERT INTO `db_personas`.`personas` (`cedula`,`nombre`,`estado`,`fecha`,`activo`) 
+VALUES ('87987497','Pepito Perez',1,NOW(),1);
+INSERT INTO `db_personas`.`personas` (`cedula`,`nombre`,`estado`,`fecha`,`activo`) 
+VALUES ('12354646','Susana Lopez',2,NOW(),0);
