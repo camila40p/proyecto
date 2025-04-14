cd68c5cc65116f4b80807ac226813d0caa46e262
Jorge Parra <jorgeparra251543@correo.itm.edu.co>
2025-04-14 14:34:24 -0500

    Finalizacion CRUD vehiculos

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #Improtar clases de otras carpetas del proyecto
 from Entidades import Conductores
+from Entidades import Vehiculos
 from Repositorios import ConductoresRepositorio
+from Repositorios import VehiculosRepositorio
 
 #Definicion metodo Main
 def main():
    
+   #--CONDUCTOR
+
    #Inicializacion de datos
    id=1
    nombre="Jorge luis parra"
@@ -37,14 +41,53 @@ def main():
 
    #Consultar
    #Creo Objeto conductor
-   conductor = Conductores.Conductores(id,nombre,cedula,telefono)
+   #conductor = Conductores.Conductores(id,nombre,cedula,telefono)
    #Creo objeto de repositorio conductores 
-   repositorio = ConductoresRepositorio.ConductoresRepositorio()
+   #repositorio = ConductoresRepositorio.ConductoresRepositorio()
    #Utilizo metodo guardar que me recibe el objeto conductor
-   repositorio.Consultar(conductor)
+   #repositorio.Consultar(conductor)
+
+   #--VEHICULO
+
+    #Inicializacion de datos
+   id=2
+   placa="GH1245"
+   tipo="Moto BMW"
+   capacidad=160
+
+   #Insertar
+   #Creo Objeto vehiculo
+   #vehiculo = Vehiculos.Vehiculos(id,placa,tipo,capacidad)
+   #Creo objeto de repositorio conductores 
+   #repositorio = VehiculosRepositorio.VehiculosRepositorio()
+   #Utilizo metodo guardar que me recibe el objeto conductor
+   #repositorio.Guardar(vehiculo)
 
 
+   #Actualizar
+   #Creo Objeto vehiculo
+   #vehiculo = Vehiculos.Vehiculos(id,placa,tipo,capacidad)
+   #Creo objeto de repositorio conductores 
+   #repositorio = VehiculosRepositorio.VehiculosRepositorio()
+   #Utilizo metodo guardar que me recibe el objeto conductor
+   #repositorio.Actualizar(vehiculo)
 
+
+   #Eliminar
+   #Creo Objeto vehiculo
+   #vehiculo = Vehiculos.Vehiculos(id,placa,tipo,capacidad)
+   #Creo objeto de repositorio conductores 
+   #repositorio = VehiculosRepositorio.VehiculosRepositorio()
+   #Utilizo metodo guardar que me recibe el objeto conductor
+   #repositorio.Eliminar(vehiculo)
+
+   #Consultar
+   #Creo Objeto vehiculo
+   #vehiculo = Vehiculos.Vehiculos(id,placa,tipo,capacidad)
+   #Creo objeto de repositorio conductores 
+   #repositorio = VehiculosRepositorio.VehiculosRepositorio()
+   #Utilizo metodo guardar que me recibe el objeto conductor
+   #repositorio.Consultar(vehiculo)
 
 
 #Llamado metodo Main
